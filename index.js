d706df074a9f269cb741f84a091cabc453192d72
Raehale <87107302+Raehale@users.noreply.github.com>
2024-08-31 12:46:06 -0600

    add new post to list of posts

--- index.js
+++ index.js
@@ -2,31 +2,36 @@
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
-        const postsArr = data.slice(0, 5)
-        let html = ""
+        const postsArr = data.slice(0, 5);
+        let html = "";
         for (let post of postsArr) {
             html += `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            `
+                <div class="post">
+                    <h3>${post.title}</h3>
+                    <p>${post.body}</p>
+                    <hr />
+                </div>
+            `;
         }
-        document.getElementById("blog-list").innerHTML = html
+        document.getElementById("blogList").innerHTML = html;
     })
 
 //Adds entered post to API and adds it to the top of the post list
-document.getElementById("new-post").addEventListener("submit", function(e) {
-    e.preventDefault()
-    const postTitle = document.getElementById("post-title").value
-    const postBody = document.getElementById("post-body").value
+document.getElementById("blogPosts").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const postTitle = document.getElementById("postTitle").value;
+    const postBody = document.getElementById("postBody").value;
     const data = {
         title: postTitle,
         body: postBody
-    }
-    
+    };
+    appendNewPost(data);
+})
+
+function appendNewPost(postData) {
     const options = {
         method: "POST",
-        body: JSON.stringify(data),
+        body: JSON.stringify(postData),
         headers: {
             "Content-Type": "application/json"
         }
@@ -35,9 +40,18 @@ document.getElementById("new-post").addEventListener("submit", function(e) {
     fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
-            /**
-             * Challenge: Update the DOM with the new blog entry
-             */
+            let html ='';
+            html = `
+                <h3>${data.title}</h3>
+                <p>${data.body}</p>
+                <hr />
+            `;
+
+            const newPost = document.createElement('div');
+            newPost.classList.add('post');
+
+            newPost.innerHTML = html;
+            document.getElementById('blogList').appendChild(newPost);
+            document.getElementById('blogList').insertBefore(newPost, document.getElementById('blogList').firstChild);
         })
-})
+}
